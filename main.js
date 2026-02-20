9ce5cf92e03163a7b20fba6805fd24019452a16f
mayahilbert <35238760+mayahilbert@users.noreply.github.com>
2026-02-20 11:53:59 -0500

    Base style and structure updates

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-var hamburgerIcon = document.getElementById("hamburger");
+var hamburgerButton = document.getElementById("hamburger");
 
-hamburgerIcon.addEventListener("click", toggleMobileMenu);
+hamburgerButton.addEventListener("click", toggleMobileMenu);
 function toggleMobileMenu() {
+    const isOpen = hamburgerButton.classList.toggle('is-open');
+  hamburgerButton.setAttribute('aria-expanded', isOpen);
   var x = document.getElementById("navLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
